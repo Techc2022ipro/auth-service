321a350b19409db98523ac6c56e5711bb326a143
sabi1125 <sabir_barahi4@hotmail.com>
2022-06-12 19:12:18 +0900

    removed typemodule

--- authserver/app/src/server.ts
+++ authserver/app/src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import Logger from './libraries/libs/Logger';
 
 const app = express();
 app.use(express.json());
 
 app.listen(1000, () => {
-    console.log('now listening to 1000')
+    Logger.info("listening to port 1000")
 })
--- authserver/scripts/entrypoint.sh
+++ authserver/scripts/entrypoint.sh
@@ -4,10 +4,10 @@ echo "Running in NODE_ENV=$NODE_ENV"
 echo "initalize environment file"
 
 echo "Running npm install ..."
-npm install --prefer-offline --no-audit
+npm install
 
 echo "Initalizing libraries modules"
 npm --prefix /app/src/libraries/ install
 
 echo "Running npm run ..."
-npm start
+npm run serve
